6cad91d99e96c9262030f4b76f30598e391f0556
thw17 <twebster17@gmail.com>
2016-11-13 13:12:49 -0700

    Wrapping all sequences in the same way

--- fasta_utils/Reduce_contigs.py
+++ fasta_utils/Reduce_contigs.py
@@ -1,4 +1,5 @@
 import argparse
+from itertools import groupby
 import subprocess
 import sys
 
@@ -24,14 +25,18 @@ def main():
 	a = subprocess.call(
 		"""{} -c fastx '{{ if (length($seq) > {} ) print ">"$name; print $seq}}' {} > tmp_large_enough.fa""".format(
 			bioawk, int(args.size) - 1, args.fasta), shell=True)
+
+
 	# Create a file of just sequence for too short scaffolds/contigs
 	a = subprocess.call(
 		"""{} -c fastx '{{ if (length($seq) < {} ) print $seq}}' {} > tmp_toosmall_seq.txt""".format(
 			bioawk, args.size, args.fasta), shell=True)
+
 	# Create a file of just sequence names for too short scaffolds/contigs
 	a = subprocess.call(
 		"""{} -c fastx '{{ if (length($seq) < {} ) print $name}}' {} > tmp_toosmall_name.txt""".format(
 			bioawk, args.size, args.fasta), shell=True)
+
 	# Concatenate too short sequences
 	with open("tmp_toosmall_seq.txt", "r") as f:
 		sequence = ""
@@ -39,11 +44,13 @@ def main():
 		for line in f:
 			sequence += line
 			lengths.append(len(line))
+
 	# Collect sequence names
 	with open("tmp_toosmall_name.txt", "r") as f:
 		names = []
 		for line in f:
 			names.append(line)
+
 	# Write fasta of supercontig
 	length = len(sequence)
 	with open("tmp_supercontig.fa", "w") as f:
@@ -53,18 +60,45 @@ def main():
 				f.write(sequence[i: i + wrap] + "\n")
 		else:
 			f.write(sequence + "\n")
+
 	# Write bed containing coordinates of contigs in the supercontig
 	with open(args.output_bed, "w") as f:
 		start = 0
 		stop = 0
 		for idx, i in lengths:
 			stop += i
-			f.write("{}\t{}\t{}\t{}".format(args.supercontig_name, start, stop, names[idx]))
+			f.write("{}\t{}\t{}\t{}".format(
+				args.supercontig_name, start, stop, names[idx]))
 			start = stop
+
+	# Ensure large enough fasta sequences are wrapped correctly (as bioawk
+	# doesn't wrap) and in a way consistent with the supercontig record
+	with open("tmp_large_enough.fa", "r") as f:
+		with open("tmp_large_enough.WRAPPED.fa") as o:
+			# the following text for parsing fasta based on Brent Pedersen's
+			# response here:  https://www.biostars.org/p/710/
+			faiter = (x[1] for x in groupby(f, lambda line: line[0] == ">"))
+			for header in faiter:
+				# drop the ">"
+				header = header.next()[1:].strip()
+				# join all sequence lines to one.
+				seq = "".join(s.strip() for s in faiter.next())
+
+				# Write output to file using same wrapping as supercontig above
+				o.write(">{}\n".format(header))
+				length = len(seq)
+				if wrap is not None:
+					for i in range(0, length, wrap):
+						o.write(sequence[i: i + wrap] + "\n")
+				else:
+					o.write(sequence + "\n")
+
 	# Concatenate fasta of sequences large enough with supercontig fasta
-	a = subprocess.call(
-		"cat tmp_large_enough.fa tmp_supercontig.fa > {}".format(
-			args.output_fasta), shell=True)
+	with open(args.output_fasta, "w") as f:
+		a = subprocess.call(
+			["cat", "tmp_large_enough.WRAPPED.fa", "tmp_supercontig.fa"],
+			stdout=f)
+
 
 def parse_args():
 	""" Parse command line arguments """
@@ -106,6 +140,22 @@ def parse_args():
 
 	return args
 
+def fasta_iter(fasta_name):
+    """
+    given a fasta file. yield tuples of header, sequence
+
+	From Brent Pedersen: https://www.biostars.org/p/710/
+    """
+    fh = open(fasta_name)
+    # ditch the boolean (x[0]) and just keep the header or sequence since
+    # we know they alternate.
+    faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
+    for header in faiter:
+        # drop the ">"
+        header = header.next()[1:].strip()
+        # join all sequence lines to one.
+        seq = "".join(s.strip() for s in faiter.next())
+        yield header, seq
 
 if __name__ == '__main__':
 	main()
