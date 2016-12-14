7a3da64d8788ad47fed1e86d84ba5ffc7a078c29
Tim Webster <twebster17@gmail.com>
2016-12-13 23:44:09 -0700

    Sorting out printing issues

--- fasta_utils/Reduce_contigs.py
+++ fasta_utils/Reduce_contigs.py
@@ -49,10 +49,10 @@ def main():
 		# Create temp fasta for large enough contigs/scaffolds
 		a = subprocess.call(
 			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name{}$seq}}' {} > tmp_pass.fa""".format(
-				bioawk, " ".join(id_list), repr("\n"), args.fasta), shell=True)
+				bioawk, " ".join(id_list), repr('"\n"'), args.fasta), shell=True)
 		print(
 			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name{}print $seq}}' {} > tmp_pass.fa""".format(
-			bioawk, " ".join(id_list), repr("\n"), args.fasta))
+			bioawk, " ".join(id_list), repr('"\n"'), args.fasta))
 
 		# Create a file of just sequence for too short scaffolds/contigs
 		a = subprocess.call(
