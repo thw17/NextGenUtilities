8531b11417c900eb753f6afdddcadcfde8cb0a98
Tim Webster <twebster17@gmail.com>
2016-12-13 23:22:12 -0700

    Fixed awk printing error

--- fasta_utils/Reduce_contigs.py
+++ fasta_utils/Reduce_contigs.py
@@ -24,7 +24,7 @@ def main():
 	if args.mode == "LENGTH":
 		# Create temp fasta for large enough contigs/scaffolds
 		a = subprocess.call(
-			"""{} -c fastx '{{ if (length($seq) > {} ) print ">"$name; print $seq}}' {} > tmp_pass.fa""".format(
+			"""{} -c fastx '{{ if (length($seq) > {} ) print ">"$name"\n"print $seq}}' {} > tmp_pass.fa""".format(
 				bioawk, int(args.size) - 1, args.fasta), shell=True)
 
 		# Create a file of just sequence for too short scaffolds/contigs
@@ -48,10 +48,10 @@ def main():
 
 		# Create temp fasta for large enough contigs/scaffolds
 		a = subprocess.call(
-			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name; print $seq}}' {} > tmp_pass.fa""".format(
+			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name"\n"$seq}}' {} > tmp_pass.fa""".format(
 				bioawk, " ".join(id_list), args.fasta), shell=True)
 		print(
-			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name; print $seq}}' {} > tmp_pass.fa""".format(
+			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name"\nprint $seq}}' {} > tmp_pass.fa""".format(
 			bioawk, " ".join(id_list), args.fasta))
 
 		# Create a file of just sequence for too short scaffolds/contigs
