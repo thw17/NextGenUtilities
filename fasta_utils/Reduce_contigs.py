924cfca76adb9d5209ca7a2bdeed34fee2c6627e
Tim Webster <twebster17@gmail.com>
2016-12-13 23:26:11 -0700

    Trying repr() for newline character

--- fasta_utils/Reduce_contigs.py
+++ fasta_utils/Reduce_contigs.py
@@ -48,11 +48,11 @@ def main():
 
 		# Create temp fasta for large enough contigs/scaffolds
 		a = subprocess.call(
-			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name"\n"$seq}}' {} > tmp_pass.fa""".format(
-				bioawk, " ".join(id_list), args.fasta), shell=True)
+			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name{}$seq}}' {} > tmp_pass.fa""".format(
+				bioawk, " ".join(id_list), repr("\n"), args.fasta), shell=True)
 		print(
-			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name"\n"print $seq}}' {} > tmp_pass.fa""".format(
-			bioawk, " ".join(id_list), args.fasta))
+			"""{} -c fastx '{{split("{}", a, " "); for (i in a) value[a[i]]; if ($name in value == 1) print ">"$name{}print $seq}}' {} > tmp_pass.fa""".format(
+			bioawk, " ".join(id_list), repr("\n"), args.fasta))
 
 		# Create a file of just sequence for too short scaffolds/contigs
 		a = subprocess.call(
